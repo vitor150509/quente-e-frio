dc11057fbcac2b1022cdfea2ef91ebf00d8bf68e
vitor150509 <vitor.ribeiro.silva15@escola.pr.gov.br>
2025-04-29 11:43:40 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,38 @@
+let x;
+let y;
+
+function setup() {
+  createCanvas(400, 400);
+  x = random(400);
+  x = int(x);
+  y = random(400);
+  y = int(y);
+}
+
+function draw() {
+  background(220);
+  x = x + random(-5,5);
+  y = y + random(-5,5);
+  x = constrain(x,0,400);
+  y = constrain(y,0,400);
+  let distancia;
+  distancia = dist(mouseX, mouseY, x, y);
+  circle(mouseX, mouseY, distancia);
+  //circle(x, y, 10);
+
+  if (distancia < 3) {
+    text("Encontrei!", 200, 200);
+    noLoop();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
